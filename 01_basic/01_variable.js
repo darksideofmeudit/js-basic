7ddbd7a669ae9477fe29ea2b490ff386278d482d
darksideofmeudit <42933017+darksideofmeudit@users.noreply.github.com>
2023-11-18 17:05:31 +0000

    DATATYPES

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "walrgmail.com"
+var accountPassword = "234222"
+accountCity="raipur"
+  
+
+// accountId=2
+
+accountEmail= "udcvkjgmail.com"
+accountPassword= "345435"
+accountCity="jaipur"
+
+console.table([accountEmail,accountPassword,accountCity])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log(3+3);
+
+let name = "udit"
+let age = 22
+let isLoggedIn = false
+
+//number 
+//bigint
+//string 
+// boolean
+// null => stand alone vale
+// undifine => 
+// symbol
+
+
+//object
+
+console.log(typeof null)
